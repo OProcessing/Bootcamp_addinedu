fbbc922c6b3cdc1ac62fef96f82cb016e42e01f7
OProcessing <hych0609@naver.com>
2024-06-18 15:42:23 +0900

    launch

--- 09.ROS/src/my_first_package/launch/turtlesim_and_teleop.launch.py
+++ 09.ROS/src/my_first_package/launch/turtlesim_and_teleop.launch.py
@@ -2,9 +2,22 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description() :
-    return LaunchDescription(
-        [
-            Node(namespace = "turtlesim", package='turtlesim', executable='turtlesim_node', output='screen'),
-            Node(namespace = "pub_cmd_vel", package='my_first_package', executable='my_publisher', output='screen'),
+    my_launch = LaunchDescription()
+    turtlesim_node = Node(
+        package='turtlesim',
+        executable='turtlesim_node',
+        output='screen'
+        parameters=[
+            {"background_r": 255},
+            {"background_g": 192},
+            {"background_b": 203},
         ]
     )
+    dist_turtle_action_node = Node(
+        package='my_first_package',
+        executable='dist_turtle_action_server',
+        output='screen',
+    )
+    my_launch.add_action(turtlesim_node)
+    my_launch.add_action(dist_turtle_action_node)
+    return my_launch
