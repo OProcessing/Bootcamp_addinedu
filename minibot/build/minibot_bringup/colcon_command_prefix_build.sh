bec39d644a64e252151fde5be56b97c89feb554b
OProcessing <hych0609@naver.com>
2024-07-01 22:30:43 +0900

    minibot upload

---
+++ minibot/build/minibot_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/han/Desktop/Han_ws/10.minibot/install/minibot_description/share/minibot_description/package.sh"
+. "/home/han/Desktop/Han_ws/10.minibot/install/minibot_gazebo/share/minibot_gazebo/package.sh"
---
+++ minibot/build/minibot_gazebo/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/han/Desktop/Han_ws/10.minibot/install/minibot_description/share/minibot_description/package.sh"
